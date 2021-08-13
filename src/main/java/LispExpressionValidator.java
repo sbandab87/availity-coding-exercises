abbe949475f47d7fde59c3abe3ddb9e0a2fa6468
sbandab87 <88802628+sbandab87@users.noreply.github.com>
2021-08-13 15:40:45 -0500

    Update LispExpressionValidator.java

--- src/main/java/LispExpressionValidator.java
+++ src/main/java/LispExpressionValidator.java
@@ -1,42 +1,44 @@
-// Java program for checking
-// balanced brackets
-
 import java.util.ArrayList;
 import java.util.List;
 import java.util.Stack;
 
+/** Validator for validating the parentheses for LISP code. */
 public class LispExpressionValidator {
 
-  public static void main(String[] args) {
+  public static void main(final String[] args) {
     final List<String> exps = new ArrayList<>();
     exps.add("(let x 2 (mult x (let x 3 y 4 (add x y)))");
     exps.add("(add x y)");
     exps.add("((mult x (let x 3 y 4 (add x y))");
     exps.add("(60 * 9 / 5) + 32");
+    exps.add("60 * 9 / 5) + 32");
+    exps.add("  ");
+    exps.add(")num()");
     exps.add("(write(+ (* (/ 9 5) 60) 32))");
 
-    exps.stream()
-        .forEach(
-            exp -> {
-              System.out.println(
-                  isValidLispExpression(exp) ? exp + ": is Balanced" : exp + ": is Not Balanced");
-            });
+    exps.forEach(
+        exp -> {
+          System.out.println(
+              isValidLispExpression(exp) ? exp + ": is Balanced" : exp + ": is Not Balanced");
+        });
   }
 
-  private static boolean isValidLispExpression(String expr) {
-    final Stack<Character> stack = new Stack<Character>();
+  private static boolean isValidLispExpression(final String expr) {
+    final Stack<Character> stack = new Stack<>();
 
     for (int i = 0; i < expr.length(); i++) {
-      char x = expr.charAt(i);
+      char c = expr.charAt(i);
 
-      if (x == '(') {
-        stack.push(x);
+      if (c == '(') {
+        stack.push(c);
         continue;
       }
 
-      if (stack.isEmpty()) return false;
+      if (stack.isEmpty()) {
+        return false;
+      }
 
-      if (x == ')') {
+      if (c == ')') {
         stack.pop();
       }
     }
